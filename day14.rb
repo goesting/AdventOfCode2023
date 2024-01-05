33802ca53b072bf20f89d3d17ac18944ce605f20
goesting <75796795+goesting@users.noreply.github.com>
2024-01-05 22:24:05 +0100

    Update day14.rb

--- day14.rb
+++ day14.rb
@@ -1,18 +1,54 @@
+def roll(tensor, direction = :none)
+    #turn the grid, so the rocks flow left every time. for easy ruby one-liner handling
+    tensor = case direction
+        when :none then roll(roll(roll(roll(tensor,:N),:W),:S),:E)
+        when :W    then tensor
+        when :E    then tensor.map &:reverse
+        when :N    then tensor.transpose
+        when :S    then tensor.reverse.transpose
+    end
+
+    #Roll balls left
+    tensor = tensor.map{|line| line.slice_when{|a,b| (a=='#') ^ (b=='#')}.map{_1.sort.reverse}.flatten} unless direction == :none
+    
+    #Undo the turning of the grid
+    tensor = case direction
+        when :N   then tensor.transpose
+        when :E   then tensor.map &:reverse
+        when :S   then tensor.transpose.reverse
+        when :W   then tensor
+        else tensor
+    end
+
+    #return the grid
+    tensor
+end
+
+#####Read input#####
 input = File.read('day14.txt').split
-input = (['#'*input[0].size] + input).map(&:chars).transpose
-puts input.inspect
-input.map!{|line|
-    current = {}
-    line.map.with_index{|c,i| 
-        if  c=='O' 
-            result = line.size - current.fetch(line[..i].rindex('#'),0) - (line[..i].rindex('#') || 0) - 1
-            current[line[..i].rindex('#')]  = current.fetch(line[..i].rindex('#'),0)+1
-            #puts current.inspect
-        else
-            result = 0
-        end
-        result
-    }
+#add #-borders to ease catching method
+input = (['#'*(input[0].size+2)] + input.map{'#'+_1+'#'} + ['#'*(input[0].size+2)]).map(&:chars)
+
+#####part 1#####
+puts roll(input,:N).map.with_index{|line,i| line.sum{|c| c=='O' ? line.size-i-1 : 0} }.sum
+
+#####part2#####
+STEPS = 1_000_000_000
+visitedStates = {} #DP dict of past configs to detect repeats. {state => i}
+
+1.upto(STEPS){|i|
+    if visitedStates.key?(input.join) #has been visited? check loop length and calc end point
+        loopLength = i - visitedStates[input.join]
+        loopRemainder = (STEPS - i) % loopLength + 1
+        loopRemainder.times{input = roll(input)} #take steps upto the state that repeats at the end
+        break
+    else #add state to visited
+        visitedStates[input.join] = i
+    end
+    input = roll(input) #keep rolling rolling rolling rolling. Move in, now move out, hand ups, now hands down. Back up back up, telle me what ya gonna do now?
 }
-puts input.inspect
-puts input.map(&:sum).sum
+
+#output
+puts input.map.with_index{|line,i| line.sum{|c| c=='O' ? line.size-i-1 : 0} }.sum
+
+
